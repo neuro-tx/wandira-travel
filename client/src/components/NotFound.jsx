27ad515fc34ddd91199fab5afc74327b8beadbad
tarek fawzy <tf5234045@gmail.com>
2025-06-02 05:47:28 +0300

    feat: finish not found page and optimize protected route

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router';
+import { useAuth } from '../contexts/shared/Auth';
+
 
 const NotFound = () => {
+  const location = useLocation();
+  const { user } = useAuth();
+  let role = user.role;
   return (
-    <div>NotFound</div>
+    <div class="min-h-screen flex flex-col justify-center items-center px-4 sm:px-6 lg:px-8">
+      <div class="max-w-md w-full space-y-8 text-center">
+        <div class="mb-8">
+          <h2 class="mt-6 text-6xl font-extrabold text-dark-100">404</h2>
+          <p class="mt-1 text-3xl font-bold text-dark-100">Page not found</p>
+          <p class="mt-2 text-sm text-gray-600 ">Sorry, we couldn't find "{location.pathname}" page that you're looking for.</p>
+        </div>
+        <div class="mt-2">
+          <Link to={`/${role !== "admin" ? "" : "admin"}`}
+            class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <svg class="mr-2 -ml-1 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12h18m-9-9l9 9-9 9" />
+            </svg>
+            {role !== "admin" ? "Go back home" : "Go back dashboard"}
+          </Link>
+        </div>
+      </div>
+      <div class="mt-2 w-full max-w-2xl">
+        <div class="relative">
+          <div class="absolute inset-0 flex items-center" aria-hidden="true">
+            <div class="w-full border-t border-gray-300 "></div>
+          </div>
+          <div class="relative flex justify-center">
+            <span class="px-2 bg-gray-100  text-sm text-gray-500">
+              If you think this is a mistake, please contact support
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/layout/AdminLayout.jsx
+++ client/src/layout/AdminLayout.jsx
@@ -4,17 +4,19 @@ import SideBar from '../components/SideBar';
 import { cn } from '../utils/util';
 import { useInterface } from '../contexts/admin/InterfaceContext';
 import { useAuth } from '../contexts/shared/Auth';
+import NotFound from '../components/NotFound';
+
 
 const AdminLayout = () => {
     const { sideBar } = useInterface();
     const {user} = useAuth();
 
     // check if the user is admin or not
-    // if not navigate him to not found page
+    // if not show not found page
     if (!user || user.role !== "admin") {
-        return <Navigate to="/not-found" />
+        return <NotFound />
     }
-    
+
     return (
         <div className='w-screen min-h-dvh'>
             <div className={cn("w-64 h-full bg-black/50 fixed top-0 z-50 shadow-100 duration-2 md:left-0 -left-full")}>
